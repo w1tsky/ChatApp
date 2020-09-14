b04f7cf2ffa6a9301d05db7a435f4ae2cd23a7cc
w1tsky <neill.white220@gmail.com>
2020-09-15 01:12:06 +0300

    actually fixed navbar layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,31 +7,49 @@ using ChatApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ChatApp.Controllers {
-    public class HomeController : Controller {
+namespace ChatApp.Controllers
+{
+    public class HomeController : Controller
+    {
         private AppDbContext _ctx;
 
-        public HomeController (AppDbContext ctx) => _ctx = ctx;
+        public HomeController(AppDbContext ctx) => _ctx = ctx;
 
-        public IActionResult Index () => View ();
+        public IActionResult Index() => View();
 
-        [HttpGet ("{id}")]
-        public IActionResult Chat (int id) 
+        [HttpGet("{id}")]
+        public IActionResult Chat(int id)
         {
             var chat = _ctx.Chats
-                .Include (x => x.Messages)
+                .Include(x => x.Messages)
                 .FirstOrDefault(x => x.Id == id);
-            return View (chat);
+            return View(chat);
         }
 
-        public async Task<IActionResult> CreateRoom (string name) {
-            _ctx.Chats.Add (new Chat {
+        public async Task<IActionResult> CreateMessage(int chatId, string message)
+        {
+            var Message = new Message {
+                ChatId = chatId,
+                Text = message,
+                Name = "Default",
+                Timestamp = DateTime.Now
+            };
+
+            _ctx.Messages.Add(Message);
+            await  _ctx.SaveChangesAsync();
+            return RedirectToAction("Chat", new {id = chatId});
+        }
+
+        public async Task<IActionResult> CreateRoom(string name)
+        {
+            _ctx.Chats.Add(new Chat
+            {
                 Name = name,
-                    Type = ChatType.Room
+                Type = ChatType.Room
             });
 
-            await _ctx.SaveChangesAsync ();
-            return RedirectToAction ("Index");
+            await _ctx.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 
